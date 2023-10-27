965cb558bd8438166632f0615d59d838e7a21e50
HaNguyenGithub <270297vh@gmail.com>
2023-10-27 10:55:39 +0700

    Add JSON SERVER

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,5 +1,60 @@
+import { useState } from "react";
 import "./SignUp.css";
+
+const initialFormState = {
+  username: "",
+  password: "",
+  email: "",
+  gender: "",
+  city: "",
+  job: [],
+};
+
 function SignUp() {
+    const [formSignUp, setFormSignUp] = useState(initialFormState);
+
+    const handleSubmitForm = (e) => {
+        addCustomer(formSignUp);
+        console.log(formSignUp);
+    }   
+
+    const addCustomer = async (data) => {
+      const response = fetch(`${process.env.REACT_APP_API_ENDPOINT}/customers`,{
+        method: 'POST',
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+        if (response.ok){
+          alert('Successfully');
+          setFormSignUp(initialFormState);
+        }
+    }
+
+    const handleChangeInput = (e) => {
+      const { name, value, type, checked } = e.target;
+      if (type === "checkbox") {
+        // Xử lý đầu vào checkbox
+        const updatedJobs = [...formSignUp.job];
+        if (checked) {
+          updatedJobs.push(value); // Thêm giá trị vào mảng
+        } else {
+          // Xóa giá trị khỏi mảng
+          const index = updatedJobs.indexOf(value);
+          if (index !== -1) {
+            updatedJobs.splice(index, 1);
+          }
+        }
+  
+        // Cập nhật trạng thái formSignUp với mảng công việc mới
+        setFormSignUp({ ...formSignUp, [name]: updatedJobs });
+      } else {
+        // Xử lý các loại đầu vào khác
+        setFormSignUp({ ...formSignUp, [name]: value });
+      }
+    }
+
   return (
     <>
       <div className="App">
@@ -7,7 +62,7 @@ function SignUp() {
           <h1>Sign Up</h1>
         </header>
         <div className="body">
-          <form className="formSignUp">
+          <form className="formSignUp" onSubmit={handleSubmitForm}>
             <div className="text">
               <div className="element">
                 <label htmlFor="username">Username:</label>
@@ -16,6 +71,8 @@ function SignUp() {
                   className="username"
                   name="username"
                   id="username"
+                  value={formSignUp.username}
+                  onChange={handleChangeInput}
                 />
               </div>
               <div className="element">
@@ -25,11 +82,13 @@ function SignUp() {
                   className="password"
                   name="password"
                   id="password"
+                  value={formSignUp.password}
+                  onChange={handleChangeInput}
                 />
               </div>
               <div className="element">
                 <label htmlFor="email">Email:</label>
-                <input type="email" className="email" name="email" id="email" />
+                <input type="email" className="email" name="email" id="email" value={formSignUp.email}  onChange={handleChangeInput}/>
               </div>
             </div>
             <div className="radio">
@@ -41,7 +100,9 @@ function SignUp() {
                   className="gender"
                   name="gender"
                   id="male"
-                  value={1}
+                  value={"male"}
+                  
+                  onChange={handleChangeInput}
                 />
                 <label htmlFor="female">Female</label>
                 <input
@@ -49,15 +110,16 @@ function SignUp() {
                   className="gender"
                   name="gender"
                   id="female"
-                  value={2}
+                  value={"female"}
+                  onChange={handleChangeInput}
                 />
               </div>
             </div>
             <div className="select">
               <div className="element">
                 <label htmlFor="city">City:</label>
-                <select id="city" name="city">
-                  <option value={0}>Choose a city</option>
+                <select id="city" name="city" onChange={handleChangeInput}>
+                  <option value={""}>Choose a city</option>
                   <option value={"HaNoi"}>HaNoi</option>
                   <option value={"DaNang"}>DaNang</option>
                   <option value={"HoChiMinh"}>HoChiMinh</option>
@@ -70,25 +132,28 @@ function SignUp() {
                 <label>Job:</label>
                 <input
                   type="checkbox"
-                  id="student"
+                  id="JAVA"
                   name="job"
                   value="student"
+                  onChange={handleChangeInput}
                 />
-                <label htmlFor="student"> Student </label>
+                <label htmlFor="JAVA"> Java </label>
                 <input
                   type="checkbox"
-                  id="doctor"
+                  id="JS"
                   name="job"
                   value="doctor"
+                  onChange={handleChangeInput}
                 />
-                <label htmlFor="doctor"> Doctor </label>
+                <label htmlFor="JS"> Javascript </label>
                 <input
                   type="checkbox"
-                  id="teacher"
+                  id="PHP"
                   name="job"
                   value="doctor"
+                  onChange={handleChangeInput}
                 />
-                <label htmlFor="teacher"> Teacher </label>
+                <label htmlFor="PHP"> PHP </label>
               </div>
             </div>
             <div className="submit">
